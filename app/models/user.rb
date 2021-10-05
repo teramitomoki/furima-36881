0a49b0bbcdf0cc89caa20c93112b8e99728e7ffe
teramitomoki <teramitomoki@gmail.com>
2021-10-05 17:44:54 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-
   has_many :items
   has_many :purchases
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password,format: {with: PASSWORD_REGEX, message: 'は半角英数字混合で設定してください'}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/, message: 'には全角文字を使用してください' } do
+  validates :last_name
+  validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カタカナを使用してください' } do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+
 end
