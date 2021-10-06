6bbf4b595b2b398b2c622a68ae2cf699f02fc384
teramitomoki <teramitomoki@gmail.com>
2021-10-06 17:06:28 +0900

    itemsモデル,テーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = item.all
+  end
+
+  def new
+    @item = item.new
+  end
+
+  def create
+    item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :estimated_shipping_date_id, :price)
   end
 end
