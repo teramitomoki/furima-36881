0a2420c6b386d4ab33f7cf8819a3310c92112286
teramitomoki <teramitomoki@gmail.com>
2021-10-06 22:43:20 +0900

    itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :estimated_shipping_date
+
+  validates :product, :description, :price, presence: true
+  validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :estimated_shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
