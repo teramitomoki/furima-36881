d3ec7e40e00c45f78b5bfa78f5ce123a5ce6555f
teramitomoki <teramitomoki@gmail.com>
2021-10-06 13:10:47 +0900

    ユーザーコントローラー削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :users
 end
