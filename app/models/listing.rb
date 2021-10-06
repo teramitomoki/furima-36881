1350fe8bfe9784791c616ca0dde4e687da97d7cf
teramitomoki <teramitomoki@gmail.com>
2021-10-06 22:40:53 +0900

    listingモデル削除

--- app/models/listing.rb
+++
@@ -1,8 +0,0 @@
-class Listing < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-
-  validates :product, :description, :price, presence: true
-
-  validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :estimated_shipping_date_id, numericality: { other_than: 1 } 
-end
