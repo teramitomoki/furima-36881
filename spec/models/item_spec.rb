ee1c821c11016add2b92dc7ad003790ff836bf20
teramitomoki <teramitomoki@gmail.com>
2021-10-07 23:36:05 +0900

    テストコード調整

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,23 +6,6 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品機能' do
-    context '商品出品できるとき' do
-      it '全ての情報が正しく入力されていれば出品できる' do
-        expect(@item).to be_valid
-      end
-      it 'priceが300円なら出品できる' do
-        @item.price = 300
-        expect(@item).to be_valid
-      end
-      it 'priceが9,999,999円なら出品できる' do
-        @item.price = 9_999_999
-        expect(@item).to be_valid
-      end
-      it 'priceが半角なら出品できる' do
-        @item.price = 300
-        expect(@item).to be_valid
-      end
-    end
     context '商品出品できないとき' do
       it 'imageがないと出品できない' do
         @item.image = nil
