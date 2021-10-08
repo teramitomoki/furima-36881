c093cbafb613923d51597b062bbfd7c2187968ac
teramitomoki <teramitomoki@gmail.com>
2021-10-09 00:15:37 +0900

    Revert "デプロイするための修正"
    This reverts commit 92e9f1a52db4ab043b2868b5acada647b5b4b17f.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.create(item_params)
     if @item.save
-      redirect_to edit_user_path(@item.user_id)
+      redirect_to items_path
     else
       render :new
     end
@@ -20,6 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :product, :description, :category_id, :condition_id, :shipping_charge_id,:prefecture_id, :estimated_shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :product, :description, :category_id, :condition_id, :shipping_charge_id,
+                                 :prefecture_id, :estimated_shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
