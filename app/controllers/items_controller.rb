ebdfcf15491bce91876d2c40bf98264e1fab02c4
teramitomoki <teramitomoki@gmail.com>
2021-10-10 15:09:23 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.create(item_params)
     if @item.save
-      redirect_to edit_user_path(@item.user_id)
+      redirect_to root_path
     else
       render :new
     end
