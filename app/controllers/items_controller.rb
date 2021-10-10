e12d16c208d30b0300e9c6895a19b64b7737783a
teramitomoki <teramitomoki@gmail.com>
2021-10-10 22:13:52 +0900

    move_to_indexにdestroy追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :set_item, only: [:show, :edit, :update, :destroy, :move_to_index]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
 
   def index
