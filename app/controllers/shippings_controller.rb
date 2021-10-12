99912b67ee0a0b04fed4bc284b56bd23d86bed67
teramitomoki <teramitomoki@gmail.com>
2021-10-12 21:57:26 +0900

    商品購入フォーム訂正

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -6,13 +6,18 @@ class ShippingsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.find(params[:item_id])
     @purchase_shipping = PurchaseShipping.new(shipping_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
   def shipping_params
-    params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
