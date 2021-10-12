a7eefc43c2e167b1d051487b34ddfc5547d491b9
teramitomoki <teramitomoki@gmail.com>
2021-10-12 21:57:26 +0900

    商品購入フォーム訂正

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,17 +1,19 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipalities, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :user_id
+    validates :item_id
     validates :address
     validates :telephone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
   end
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
-    purchase =  Purchase.create(item_id: item.id, user_id: user.id)
+    purchase =  Purchase.create(user_id: user_id, item_id: item_id)
     Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, telephone_number: telephone_number	, purchase_id: purchase.id)
   end
 
