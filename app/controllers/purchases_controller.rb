e3cdc3652924c3fb58d11b41c9b5d18448766ef2
teramitomoki <teramitomoki@gmail.com>
2021-10-13 20:25:13 +0900

    ルボコップ実行

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,31 @@
+class PurchasesController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_shipping = PurchaseShipping.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      # Payjp.api_key = "sk_test_26d8d68ecf668cc6b1fb58c8"
+      # Payjp::Charge.create(
+      #   amount: purchase_shipping_params[:price],
+      #   card: purchase_shipping_params[:token],
+      #   currency: 'jpy'
+      # )
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
+  end
+end
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,20 +1,21 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id, :item_id,
+                :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :municipalities, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :municipalities
+    validates :address
     validates :user_id
     validates :item_id
-    validates :address
-    validates :telephone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0 }
   end
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
-    purchase =  Purchase.create(user_id: user_id, item_id: item_id)
-    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, telephone_number: telephone_number	, purchase_id: purchase.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address,
+                    building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
-
 end
